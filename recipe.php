b5ce26d20b0ef23184f802e8be2aa31f0eae0c87
Johannes Wachter <johannes.wachter@massiveart.com>
2017-07-03 17:11:49 +0200

    added check for existing releases (#1)

--- recipe.php
+++ recipe.php
@@ -7,25 +7,40 @@ set('nanbando_backup_options', '');
 set('nanbando_push', true);
 set('nanbando', true);
 
-task('nanbando:reconfigure', function () {
-    if (get('nanbando')) {
-        run('cd {{current_path}} && {{bin/php}} {{bin/nanbando}} reconfigure');
+set('nanbando_enabled', function () {
+    if (!get('nanbando')) {
+        return false;
     }
+
+    $releases = get('releases_list', []);
+
+    return 0 < count($releases);
+});
+
+task('nanbando:reconfigure', function () {
+    if (!get('nanbando_enabled')) {
+        return;
+    }
+
+    run('cd {{current_path}} && {{bin/php}} {{bin/nanbando}} reconfigure');
 })->desc('Reconfigure nanbando');
 
 task('nanbando:backup', function () {
-    if (get('nanbando')) {
-        run('cd {{current_path}} && {{bin/php}} {{bin/nanbando}} backup {{nanbando_backup_options}}');
+    if (!get('nanbando_enabled')) {
+        return;
     }
+
+    run('cd {{current_path}} && {{bin/php}} {{bin/nanbando}} backup {{nanbando_backup_options}}');
 })->desc('Create backup');
 
 task('nanbando:push', function () {
-    if (get('nanbando') && get('nanbando_push')) {
-        run('cd {{current_path}} && {{bin/php}} {{bin/nanbando}} push');
+    if (!get('nanbando_push') || !get('nanbando_enabled')) {
+        return;
     }
+
+    run('cd {{current_path}} && {{bin/php}} {{bin/nanbando}} push');
 })->desc('Push backup to remote storage');
 
 before('nanbando:backup', 'nanbando:reconfigure');
 after('nanbando:backup', 'nanbando:push');
 before('deploy:prepare', 'nanbando:backup');
-
